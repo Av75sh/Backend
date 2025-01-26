4fc6cdf7e8eb8cc6efd85c3ce097e014d7d38da5
Avinash <avinashlpu75@gmail.com>
2025-01-26 19:05:35 +0000

    Jan 27, 2025, 12:35 AM

--- 2NPM/4ejs.js
+++ 2NPM/4ejs.js
@@ -3,6 +3,17 @@ const app = express()
 
 app.set("view engine", 'ejs')
 
+app.use((req, res, next) => {
+    console.log("Middleware")
+
+    const a = 10;
+    const b = 11;
+
+    console.log(a+b)
+    return next()
+
+})
+
 app.get('/home', (req, res) => {
     res.send("Hello World")
 })
