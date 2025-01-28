43e14c028074426790f02122a5fc0d8b1b802742
Avinash <avinashlpu75@gmail.com>
2025-01-28 05:31:30 +0000

    Jan 28, 2025, 11:01 AM

--- 2NPM/1app.js
+++ 2NPM/1app.js
@@ -12,17 +12,28 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))                  // for css files
 
-
 app.set("view engine", 'ejs');
 app.get('/', (req,res) => {
     res.render('3form')
 })
 
+ 
+app.post('/post-form-data', async (req, res) => {            // make asynchronus to synchronus
+    const {username, email, password} = req.body
 
-app.post('/post-form-data', (req, res) => {
-    console.log(req.body);
-    res.send('Data recieved');
+    await userModel.create({
+        username: username,
+        email: email,
+        password: password
+    })
+
+
+    res.send('Register user');
 })
+
+
+
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
