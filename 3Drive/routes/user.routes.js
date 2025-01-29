c5bb426b70f24c8bc7fd8aae8127a8e1a3cd9f03
Avinash <avinashlpu75@gmail.com>
2025-01-29 06:30:48 +0000

    Jan 29, 2025, 12:00 PM

--- 3Drive/routes/user.routes.js
+++ 3Drive/routes/user.routes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//Middleware
-router.use(express.json());
-router.use(express.urlencoded({extended: true}));
 
 
 router.get('/register', (req,res) => {
