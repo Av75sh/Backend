6f18ea07e208581eee19bbce90667b3a3699abcb
Avinash <avinashlpu75@gmail.com>
2025-01-30 02:35:39 +0000

    Jan 30, 2025, 8:05 AM

--- 3Drive/routes/user.routes.js
+++ 3Drive/routes/user.routes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { body } = require('express validator');
-
-
+const { body, validationResult } = require('express validator');
 
 router.get('/register', (req,res) => {
     res.render('register')
 })
 
 router.post('/register',
-    
-    
+    body('email').trim().isEmail().isLength({ min:5 }),
+    body('useranme').trim().isLength({ min:5 }),
+    body('password').trim().isLength({ min: 5}),
     
     (req, res) => {
-    console.log(req.body)
-    res.send('User registered')
+        const errors = validationResult(req);
+        console.log(errors);
+        res.send(errors);
 })
 module.exports = router;
 
