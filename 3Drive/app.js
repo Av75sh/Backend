f6236f6f6c43cf0546e98eb3b9ff78fa9ea55275
Avinash <avinashlpu75@gmail.com>
2025-01-31 14:44:15 +0000

    Jan 31, 2025, 8:14 PM

--- 3Drive/app.js
+++ 3Drive/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+dotenv.config();
 const connectToDB = require('./config/db');
 connectToDB();
 
 const useRouter = require('./routes/user.routes')
 const app = express();
 app.use(morgan('dev'));
-dotenv.config();
+
 
 
 
--- 3Drive/config/db.js
+++ 3Drive/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectToDB(){
     mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log('COnnected to DB');
+        console.log('Connected to DB');
     })
 }
 
